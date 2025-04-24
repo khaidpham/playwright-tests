25bb4c01e2758223cc8844ea20fb2f4a509442d4
Khai Pham <khaipham@gmail.com>
2025-04-24 12:50:08 -0400

    update visual test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,49 +1,53 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Home page no auth', () => {
+test.describe('Home page', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://practicesoftwaretesting.com/');
   });
 
-  test('check sign in link', async ({ page }) => {
-    // Check Sing In link is present
-    await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
-  });
-
-  test('check page title', async ({ page }) => {
-    // Verify the page title
+  test('should have correct title', async ({ page }) => {
     await expect(page).toHaveTitle(/Practice Software Testing/);
+  });  
+  
+  test('should have a sign in link enabled', async ({ page }) => {
+    await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
+    await expect(page.getByTestId('nav-sign-in')).toBeEnabled();
   });
 
-  test('content grid loads 9 items', async ({ page }) => {
-    // Check the count of items on the page
+  test('should navigate to the contact page', async ({ page }) => {
+    await page.click('text=Contact');
+    await expect(page).toHaveURL('https://practicesoftwaretesting.com/contact');
+    await expect(page.locator('h3')).toHaveText('Contact');
+  });
+
+  test('should have a content grid that loads 9 items by default', async ({ page }) => {
     const productGrid = page.locator('.col-md-9');
     await expect(productGrid.getByRole('link')).toHaveCount(9);
   });
 
-  test('check search results', async ({ page }) => {
-    // Search for Thor Hammer
+  test('should search for a specific product', async ({ page }) => {
     await page.getByTestId('search-query').fill('Thor Hammer');
     await page.getByTestId('search-submit').click();
     await expect(page.getByTestId('search-caption')).toHaveText('Searched for: Thor Hammer');
-    // Visual test search results, with an allow different pixel ratio
-    await expect(page).toHaveScreenshot('thorHammer.png', { maxDiffPixelRatio: 0.2 });
+    const searchResults = page.locator('a.card');
+    console.info('searchResults', searchResults);
+    await expect(searchResults).toHaveCount(1);
+    // Check the count of items on the page
+    const results = await page.$$('[data-test="product-name"]');
+    console.info('results', results);
+    expect(results.length).toBeGreaterThan(0);
+    // Check the first result contains 'hammer'
+    const firstResultText = await results[0].textContent();
+    expect(firstResultText.toLowerCase()).toContain('hammer');
   });
 
-  // Home page visual test is flaky due to load time
-  // test('visual test', async ({ page }) => {
-  //   // Verify screenshot
-  //   await expect(page).toHaveScreenshot('homepage.png');
-  // });
-});
-test.describe('Home page customer 01 auth', () => {
-  test.use({ storageState: '.auth/customer01.json'  });
-  test.beforeEach(async ({ page }) => {
-    await page.goto('https://practicesoftwaretesting.com/');
-  });
-
-  test('check signed in customer info', async ({ page }) => {
-    // Check Signed in customer info
-    await expect(page.locator('[data-test="nav-menu"]')).toContainText('Jane Doe')
+  test('should match the base homepage screenshot', async ({ page }) => {
+    // Wait for content to load completely
+    await page.waitForLoadState('networkidle');
+    // Verify screenshot
+    // await expect(page).toHaveScreenshot('homepage.png');
+    await expect(page).toHaveScreenshot('homepage.png', {
+      maxDiffPixelRatio: 0.02
+    });
   });
 });
